64be96e14efe5af164a4f493923b6dbb3c1ade8e
Hsapex <marvelsrael54@gmail.com>
2025-01-11 14:32:18 +0100

    Update config.js

--- config.js
+++ config.js
@@ -8,18 +8,18 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //======= Change settings =======\\
-global.owner = ["2347036214381"]
-global.botname = ["Arch Md"]
-global.simbol = "♘"
+global.owner = ["2348109547767"]
+global.botname = ["𝕐𝔹ℕ𝕃🤍"]
+global.simbol = "🤍"
 global.typeMenu = 'v5' //don't change 
 global.anticall = false // set true to turn anticall always on
-global.xprefix = '.' // your desired prefix
+global.xprefix = ',' // your desired prefix
 global.presence = 'available' // available to show always online // unavailable to show not oniline //composing to show fake typing //recording to show fake recording audio
 global.thumb = 'https://files.catbox.moe/9nrqqg.jpg' // your menu image
 global.timezone = 'Africa/Lagos' // input timezone codrectly with continuent then / city if input wrongly might cause error
 //======= Don't change =======\\
 
-global.prefa = ['.']
+global.prefa = [',']
 
 //======= Don't touch =======\\
 global.msg = {
